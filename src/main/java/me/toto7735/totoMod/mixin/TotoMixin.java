06915ef78ce21bc6a13a0dae23f9ea4583d6fb5f
toto7735 <totoserver@naver.com>
2022-02-14 12:06:29 +0900

    Initial commit

---
+++ src/main/java/me/toto7735/totoMod/mixin/TotoMixin.java
@@ -0,0 +1,16 @@
+package me.toto7735.totoMod.mixin;
+
+import me.toto7735.totoMod.TotoMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TotoMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TotoMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
